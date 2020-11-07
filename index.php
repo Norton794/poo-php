a5fd553a4242a6252af0aa1e540373301a2844d9
Norton <nortonsantos79@gmail.com>
2020-11-07 13:30:07 -0200

    Modifiquei o arquivo index.php novamente

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title>Classe, Atributos e Métodos</title>
+        <title>Classe, Atributos e Métodos - Visibilidade</title>
     </head>
     <body>
         <?php
         require_once './Conta.php';  //Inclui a classe Conta
         $cnt = new Conta();   //Instancia um objeto da classe Conta
-        $cnt->agencia = '7768-4'; //Atribui a Agência
-        $cnt->conta = '11229-8'; //Atribui o número da conta
-        $cnt->saldo = 0.0; //Saldo inicial
+        $cnt->setAgencia('7768-4'); //Atribui a Angência
+        $cnt->setConta('11229-8'); //Atribui o número da conta
+        $cnt->setSaldo(0.0); //Saldo inicial
         
-        echo "Saldo Atual: {$cnt->saldo()}<br>";
+        echo "Saldo Atual: {$cnt->getSaldo()}<br>";
         $cnt->depositar(100.0);
-        echo "Saldo Atual: {$cnt->saldo()}<br>";
+        echo "Saldo Atual: {$cnt->getSaldo()}<br>";
         $cnt->sacar(10.0);
-        echo "Saldo Atual: {$cnt->saldo()}<br>";
+        echo "Saldo Atual: {$cnt->getSaldo()}<br>";
         $cnt->sacar(100.0);
-        echo "Saldo Atual: {$cnt->saldo()}<br>";
+        echo "Saldo Atual: {$cnt->getSaldo()}<br>";
         
         ?>
     </body>
