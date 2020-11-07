bcdbc311659fde4e8b76b0745e101bd28c745912
Norton <nortonsantos79@gmail.com>
2020-11-07 13:47:00 -0200

    Alterei o Conta.php

--- Conta.php
+++ Conta.php
@@ -5,6 +5,18 @@ abstract class Conta {
     private $conta;
     private $saldo;
     //métodos
+//
+    function __construct($agencia,$conta) {
+        $this->agencia = $agencia;
+        $this->conta = $conta;
+        echo '<br>Contrutor da Classe executado.<br>';
+      }
+     
+     function __destruct() {
+        echo '<br>Método destruct executado!<br>';
+      }
+//     
+
     public function depositar($valor){
         $this->saldo += $valor;
       }
