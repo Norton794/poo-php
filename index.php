aef373c8391ca03718ab0b62d6f0aeb07932f30b
Norton <nortonsantos79@gmail.com>
2020-11-07 13:50:53 -0200

    Alterei o index para instanciar o construtor e o destrutor

--- index.php
+++ index.php
@@ -11,11 +11,13 @@
         require_once './ContaPoupanca.php';  //Inclui a classe ContaPoupanca
         require_once './ContaSalario.php';  //Inclui a classe ContaSalario
         
-        $cntCorr = new ContaCorrente(); //Instancia um objeto da classe ContaCorrente
-        $cntCorr->setAgencia('7768-4'); //Atribui a Angência
-        $cntCorr->setConta('11229-8'); //Atribui o número da conta
+        $cntCorr = new ContaCorrente('7768-4', '11229-8'); //Instancia um objeto da classe ContaCorrente
+        //$cntCorr->setAgencia('7768-4'); //Atribui a Angência
+        //$cntCorr->setConta('11229-8'); //Atribui o número da conta
         $cntCorr->setSaldo(0.0); //Saldo inicial
         
+        echo "Agencia: {$cntCorr->getAgencia()}<br>";
+        echo "Conta: {$cntCorr->getConta()}<br>";
         echo "Saldo Atual: {$cntCorr->getSaldo()}<br>";
         $cntCorr->depositar(100.0);
         echo "Saldo Atual: {$cntCorr->getSaldo()}<br>";
